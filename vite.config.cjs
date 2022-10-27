a3b52e5f7b81390f361009f098da0129735ef7d4
kindtiger <seonbeom2@gmail.com>
2022-10-27 21:26:42 +0900

    환경변수 제거

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: './public',
-  base: process.env.NODE_ENV.includes('production') ? '/Bridge/build/' : '/',
+  // base: process.env.NODE_ENV.includes('production') ? '/Bridge/build/' : '/',
+  base: '/Bridge/build/',
   build: {
     outDir: '../build',
   },
