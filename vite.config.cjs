0d2922264a350902f542425d77f619f5e4857af3
kindtiger <seonbeom2@gmail.com>
2022-10-27 14:15:09 +0900

    build 경로 추가

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   build: {
     outDir: '../build',
   },
-  base:'https://simseonbeom.github.io/Bridge/'
+  base:'https://simseonbeom.github.io/Bridge/build'
   // publicDir: 'assets',
 };
